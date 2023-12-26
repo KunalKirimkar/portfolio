fd818a19abf3d92728e0a074ddf9a3dff95e1907
KunalKirimkar <KunalKirimkar@users.noreply.github.com>
2023-12-26 13:18:14 +0000

    deploy: 7d37619a9585a08ed20a433431565492bf34d2df

--- default-07076E3AA5AB7BE9538378DBCBFE5F57F78CCC363ACDFA4E77EB8ED842E5AA65.js
+++ default-07076E3AA5AB7BE9538378DBCBFE5F57F78CCC363ACDFA4E77EB8ED842E5AA65.js
@@ -1939,29 +1939,25 @@ class Node2 {
         }
     }
     attachExternalCss(css) {
-        if (doubleBuffering) {
-            let css_tag = document.createElement('link');
-            css_tag.rel = 'stylesheet';
-            css_tag.type = 'text/css';
-            css_tag.href = css;
+        let css_tag = document.createElement('link');
+        css_tag.rel = 'stylesheet';
+        css_tag.type = 'text/css';
+        css_tag.href = css;
 
-            let head = document.head || document.getElementsByTagName("head")[0];
-            if (!fastn_dom.externalCss.has(css)){
-                head.appendChild(css_tag);
-                fastn_dom.externalCss.add(css);
-            }
+        let head = document.head || document.getElementsByTagName("head")[0];
+        if (!fastn_dom.externalCss.has(css)){
+            head.appendChild(css_tag);
+            fastn_dom.externalCss.add(css);
         }
     }
     attachExternalJs(js) {
-        if (doubleBuffering) {
-            let js_tag = document.createElement('script');
-            js_tag.src = js;
+        let js_tag = document.createElement('script');
+        js_tag.src = js;
 
-            let head = document.head || document.getElementsByTagName("head")[0];
-            if (!fastn_dom.externalJs.has(js)){
-                head.appendChild(js_tag);
-                fastn_dom.externalCss.add(js);
-            }
+        let head = document.head || document.getElementsByTagName("head")[0];
+        if (!fastn_dom.externalJs.has(js)){
+            head.appendChild(js_tag);
+            fastn_dom.externalCss.add(js);
         }
     }
     attachColorCss(property, value, visited) {
